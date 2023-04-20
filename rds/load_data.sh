b456d0ae76bb4239ce1090ce899f3c440b18e71e
George730 <changly@connect.hku.hk>
2023-04-20 16:28:17 -0400

    update load data

--- rds/load_data.sh
+++ rds/load_data.sh
@@ -1,30 +1,30 @@
 for f in ./address/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE address COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE address COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
 
 for f in ./email/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE email COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE email COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
 
 for f in ./email_type/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE email_type COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE email_type COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
 
 for f in ./phone_number/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE phone_number COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE phone_number COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
 
 for f in ./phone_number_type/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE phone_number_type COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE phone_number_type COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
 
