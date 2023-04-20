3ff13bbdc33f789de35891d4383f5aa660273bea
George730 <changly@connect.hku.hk>
2023-04-20 16:12:04 -0400

    update load data

--- rds/load_data.sh
+++ rds/load_data.sh
@@ -30,24 +30,24 @@ done
 
 for f in ./role_profile/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE role_profile COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE role_profile COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
 
 for f in ./role_profile_type/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE role_profile_type COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE role_profile_type COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
 
 for f in ./user/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE user COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE user COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
 
 for f in ./user_profile/*
 do
-    mysql --local-infile=1 -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE user_profile COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
+    mysql --local-infile=1 --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;SET SQL_MODE='ALLOW_INVALID_DATES';" -h dms-sample.cu144p4hztzw.us-east-1.rds.amazonaws.com -P 3306 -u admin -pchangeit aspen -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE user_profile COLUMNS terminated by ',' LINES TERMINATED BY '\n';"
 echo "Done: '"$f"' at $(date)"
 done
